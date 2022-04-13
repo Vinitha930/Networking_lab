cdd263eb1b201ab61765044172f295f8f5bd644e
Vinitha930 <96111104+Vinitha930@users.noreply.github.com>
2022-04-13 15:05:39 +0530

    compare 3 numbers using ifelif loop

---
+++ ifelif.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a=20
+b=10
+
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+elif [ $a -gt $b ]
+then
+   echo "a is greater than b"
+elif [ $a -lt $b ]
+then
+   echo "a is less than b"
+else
+   echo "None of the condition met"
+fi
